c086aaff27f2a64d8902b51c73a7ce8217cd072d
trilux35 <alfredo30c@gmail.com>
2025-10-30 08:59:09 -0500

    Add files via upload

---
+++ lecturas-datos.js
@@ -0,0 +1,10 @@
+window.lecturas = {
+  "2025-10-21": {"libro": "Josué", "capitulo": 1, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-1"},
+  "2025-10-22": {"libro": "Josué", "capitulo": 2, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-2"},
+  "2025-10-23": {"libro": "Josué", "capitulo": 3, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-3"},
+  "2025-10-24": {"libro": "Josué", "capitulo": 4, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-4"},
+  "2025-10-25": {"libro": "Josué", "capitulo": 5, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-5"},
+  "2025-10-26": {"libro": "Josué", "capitulo": 6, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-6"},
+  "2025-10-27": {"libro": "Josué", "capitulo": 7, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-7"},
+  "2025-10-28": {"libro": "Josué", "capitulo": 8, "enlace": "https://tecnobiblia21.blogspot.com/2025/10/libro-de-josue.html#capitulo-8"}
+};
